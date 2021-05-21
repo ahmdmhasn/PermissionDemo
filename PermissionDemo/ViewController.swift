316c3c098e3e6a6deb6b6879627ba5a281567260
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2021-05-21 20:03:24 +0200

    Initial Commit

---
+++ PermissionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PermissionDemo
+//
+//  Created by Ahmed M. Hassan on 21/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
